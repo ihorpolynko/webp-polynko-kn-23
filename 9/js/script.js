2e72263920c32def6f13e9675a4846ef34a769d3
Ігор Полинько <igor.polynko@gmail.com>
2025-04-21 21:01:45 +0300

    Added LB9

---
+++ 9/js/script.js
@@ -0,0 +1,11 @@
+// 1. Перетворення числа на рядок
+let price = 12;
+let priceStr = price.toString();
+console.log(priceStr);
+console.log(typeof priceStr);
+
+// 2. Перетворення рядка на число
+let temperature = "25.5";
+let temperatureNum = parseFloat(temperature);
+console.log(temperatureNum);
+console.log(typeof temperatureNum);
